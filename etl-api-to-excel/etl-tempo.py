2921caaab914e21660a75416996e34f7d653b0a4
Giovana Gezuato <giovanagezuato@gmail.com>
2025-07-27 12:07:46 -0300

    criação das colunas clima e perido_dia + exportação excel

--- etl-api-to-excel/etl-tempo.py
+++ etl-api-to-excel/etl-tempo.py
@@ -1,5 +1,7 @@
 import requests
 import pandas as pd
+import numpy as np
+import os
 
 
 # Extração
@@ -18,9 +20,9 @@ def obtendo_dataframe(dados):
     # Renomeando as colunas
     df = pd.DataFrame({
         'horários': dados['time'],
-        'temperatura (°C)': dados['temperature_2m'],
-        'umidade relativa do ar (%)': dados['relative_humidity_2m'],
-        'velocidade do vento (km/h)': dados['windspeed_10m']
+        'temperatura': dados['temperature_2m'],
+        'umidade_relativa_ar': dados['relative_humidity_2m'],
+        'velocidade_vento': dados['windspeed_10m']
     })
 
     df.set_index('horários', inplace=True)  # Os horários serão os índices
@@ -33,10 +35,43 @@ def obtendo_dataframe(dados):
     hoje = pd.Timestamp.now(tz='America/Sao_Paulo').normalize()
     fim = hoje + pd.Timedelta(days=7)
     df = df.loc[(df.index >= hoje) & (df.index < fim)]
+
     return df
+
+
+# Criando colunas clima (classifica o clima) e periodo_dia (classifica os horários)
+def classifica_clima_e_horarios(df):
+    # Clima
+    condicoes_clima = [df['temperatura'] < 15, (df['temperatura'] >= 15) & (df['temperatura'] <= 25)]
+    classificacoes_clima = ['Frio', 'Ameno']
+
+    df['clima'] = np.select(condicoes_clima, classificacoes_clima, default='Quente')
+
+    # Horários
+    condicoes_horarios = [(df.index.hour >= 6) & (df.index.hour < 12),
+                          (df.index.hour >= 12) & (df.index.hour < 18),
+                          (df.index.hour >= 18) & (df.index.hour < 23)]
+    classificacoes_horarios = ['Manhã', 'Tarde', 'Noite']
+
+    df['periodo_dia'] = np.select(condicoes_horarios, classificacoes_horarios, 'Madrugada')
+
+    return df
+
+
+def exportar(arquivo, caminho, df):
+    try:
+        df.index = df.index.tz_convert(None)
+        df.to_excel(os.path.join(caminho, arquivo), index=True)
+    except Exception as e:
+        print(f'Não foi possível exportar o arquivo! Erro: {e}')
+    else:
+        print(f'Arquivo salvo como {arquivo}!')
 
 
 url = "https://api.open-meteo.com/v1/forecast?latitude=-23.55&longitude=-46.63&hourly=temperature_2m,relative_humidity_2m,windspeed_10m"
 dados = extrair(url)
 df = obtendo_dataframe(dados)
-print(df)
+df_final = classifica_clima_e_horarios(df)
+arquivo_output = 'previsao_tempo.xlsx'
+caminho_output = 'C:/Users/Giovana/GithHub repositorios/etls/etl-api-to-excel/dados/output/'
+exportar(arquivo_output, caminho_output, df_final)
